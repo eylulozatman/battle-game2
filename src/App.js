50b6dfb7ee78585349c70ca72af5c2ed8809a904
Eylül Özatman <74192529+eylulozatman@users.noreply.github.com>
2025-01-08 14:17:27 +0300

    router changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';  // Ensure Navigate is imported
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // HashRouter'ı kullanıyoruz
 import HomePage from './homepage';
 import Combat from './combat';
 
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/homepage" element={<HomePage />} />
         <Route path="/combat" element={<Combat />} />
-        <Route path="/" element={<Navigate to="/homepage" />} />  {/* Fix for 'Navigate' is not defined */}
+        <Route path="/" element={<Navigate to="/homepage" />} />
       </Routes>
     </Router>
   );
